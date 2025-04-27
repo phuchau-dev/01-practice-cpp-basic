3578c6ea2e8885d23fdd20abdfd910054b5f1cca
phuchau-dev <dophuchau.dev.@gmail.com>
2025-04-27 13:44:21 +0700

    Fix Main.cpp

---
+++ applications/Addition.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+class Addition {
+public:
+	static void main();
+};
+#pragma once
---
+++ applications/AdditionExerciseMain.cpp
@@ -0,0 +1,7 @@
+﻿#include "AdditionExerciseMain.h"
+
+void AdditionExerciseMain::execute() {
+    PrintLine::printLine("bat dau bai Addition");
+    Addition::main();
+    PrintLine::printLine("ket thuc bai Addition");
+}
